a378ec58906e1d35ba9839de5a5c428883ab7453
SZLforGithub <chenchiyu8289@gmail.com>
2018-01-04 00:22:05 +0800

    Initial Commit

---
+++ imeiFinal/ItemTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ItemTableViewCell.swift
+//  imei
+//
+//  Created by user_05 on 2017/6/20.
+//  Copyright © 2017年 user_05. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
